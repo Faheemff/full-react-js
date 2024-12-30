c197b33177073addf7cf1ed12375277fa8186a8a
Faheemff <mfaheemweb@gmail.com>
2024-12-30 21:18:52 +0500

    add

--- src/Rendering-in-react/Mounting.jsx
+++ src/Rendering-in-react/Mounting.jsx
@@ -1,31 +1,11 @@
-import React from 'react'
+import { Component } from 'react'
 
-
-class MountingExample extends React.Component {
-  constructor(props) {
-    super(props);
-    this.setState = {message: 'Mounting example'}
-    console.log('1. Constructing Mounting Example');
-  }
-
-
-  static getDerivedStateFromProps(props, state) {
-    console.log(props,state,'2. getDerivedStateFromProps called');
-    return null; // State ko change mat karo
-  }
-
+export default class Mounting extends Component {
   render() {
-    console.log('3. Render called');
-    return <h1>{this.state.message}</h1>;
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    )
   }
-
-
-  componentDidMount() {
-    console.log('4. ComponentDidMount called');
-    // API call ya timer yahan set hota hai
-  }
-
-  
 }
-
-export default MountingExample;
