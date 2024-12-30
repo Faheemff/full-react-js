e774d9536ea444b0b1a4c2ecebc3b1ebc9a36f33
Faheemff <mfaheemweb@gmail.com>
2024-12-30 19:57:45 +0500

    add

--- src/Rendering-in-react/ComLifecycle.jsx
+++ src/Rendering-in-react/ComLifecycle.jsx
@@ -1,7 +1,17 @@
 const ComLifecycle = () => {
   return (
     <div>
-      
+      {/*react lifecycle mai react component mai ek different process hota hai 
+      ya process teen major phases mai divide hota hai: jes mai number 1
+      mounting number 2 mai updating and number 3 mai unmounting hota hai */}
+
+
+constructor(props) {
+  super(props);
+  this.state = { counter: 0 };
+}
+
+
     </div>
   )
 }
