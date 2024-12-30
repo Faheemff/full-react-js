5b020c9d978d72c72800fb4d59b7f27ea0d3268d
Faheemff <mfaheemweb@gmail.com>
2024-12-30 22:02:38 +0500

    add

--- src/Rendering-in-react/UnMounting.jsx
+++ src/Rendering-in-react/UnMounting.jsx
@@ -1,6 +1,12 @@
 import { Component } from 'react'
 
 export default class UnMounting extends Component {
+
+
+  componentDidMount() {
+    console.log('Component did mount in Unmounting')
+  }
+
   render() {
     return (
       <div>
