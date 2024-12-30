6e65dc322912bc501eb19adf87c931c3a691dac2
Faheemff <mfaheemweb@gmail.com>
2024-12-30 21:50:28 +0500

    add

--- src/Rendering-in-react/Mounting.jsx
+++ src/Rendering-in-react/Mounting.jsx
@@ -2,16 +2,28 @@ import { Component } from 'react'
 
 export default class Mounting extends Component {
 
+  constructor (props){
+    super(props);
+    this.state = {count: 0}
+  }
+
 
   componentDidMount() {
     console.log('Component mounted')
     // Perform any necessary setup or fetch data here
   }
 
+
+  incrementCount = ()=> {
+    this.setState({count: this.state.count + 1})
+  }
+
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <h1>Mounting Component  {`${this.state.count}`}</h1>
+
+        <button onClick={this.incrementCount}>click</button>
       </div>
     )
   }
