48fdfedad26643e8e754c44f0a898c0a28e67ed5
Faheemff <mfaheemweb@gmail.com>
2024-12-31 21:02:41 +0500

    Add

---
+++ src/Components/PropsVSState.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const PropsVSState = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PropsVSState
