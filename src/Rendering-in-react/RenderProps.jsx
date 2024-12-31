59415175bfc533b5f7e4b5817922803a35d1c2a3
Faheemff <mfaheemweb@gmail.com>
2024-12-31 21:35:55 +0500

    add

--- src/Rendering-in-react/RenderProps.jsx
+++ src/Rendering-in-react/RenderProps.jsx
@@ -1,12 +1,27 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
+import { Component } from 'react'
 
-const RenderProps = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+export default class RenderProps extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {x: 0, y: 0};
+    };
+
+
+    handleMouseMove = (event)=> {
+        this.setState({
+            x: event.clientX,
+            y: event.clientY
+        })
+    }
+
+
+
+  render() {
+    return (
+      <div onMouseMove={this.handleMouseMove} style={{height: '100vh'}}>
+        {this.props.render(this.state)}
+      </div>
+    )
+  }
 }
 
-export default RenderProps
